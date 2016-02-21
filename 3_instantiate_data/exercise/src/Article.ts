52eb89b69b65cedeb0e2f15c5f6968bcb6560e14
Daniel Hultqvist <daniel.hultqvist@omegapoint.se>
2016-02-21 20:32:27 +0100

    Removed solution code from exercise

--- 3_instantiate_data/exercise/src/Article.ts
+++ 3_instantiate_data/exercise/src/Article.ts
@@ -10,19 +10,9 @@ class TextArticle implements Article {
 
 class ImageArticle implements Article {
     constructor(public title: string, public author: string, public content: string, public uri: URI) {    }
-    
-    static fromJSON(data: any) : ImageArticle {
-        let article = <ImageArticle>data; // We dont have any functions in this class, so we can direct cast most fields, except URI
-        article.uri = new URI(data.uri); // So set it explicitly
-        return article;
-    }
 }
 
 class VideoArticle implements Article {
     constructor(public title: string, public author: string, public content: string, private uri: URI) {    }   
     getURI() : URI { return this.uri; }
-    
-    static fromJSON(data: any) : VideoArticle {
-        return new VideoArticle(data.title, data.author, data.content, new URI(data.uri)); // we could also create a new using constructor. 
-    }
 }
