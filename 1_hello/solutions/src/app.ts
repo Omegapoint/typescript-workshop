95ff93d620a70211a088f0ee86339c34f24f6560
Daniel Hultqvist <daniel.hultqvist@omegapoint.se>
2016-03-01 10:44:11 +0100

    Updated exercise 1 according to comments

--- 1_hello/solutions/src/app.ts
+++ 1_hello/solutions/src/app.ts
@@ -1,8 +1,20 @@
+// Typesafe function that only allows string values
 function printHelloWorld(person: string, element: string) {
     document.getElementById(element).innerHTML = "Hello " + person;
 }
 
+// Create a user object of type string
 var user: string = "Daniel";
+
+// Selector for find HTML element to replace text in.
 var element: string = "welcome-text";
 
+// Bonus variable for learning
+var age: number = 12; 
+
+// Run typesafe function that replaces HTML with input.
 printHelloWorld(user, element);
+
+// What happens when you attempt to use
+// printHelloWorld(age, element)
+// Why? How would they differ after compilation to Javascript?
